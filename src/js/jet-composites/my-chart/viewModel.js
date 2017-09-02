1f5c190633015994507fecec5463d48edcafca54
peppertech <peppertech@gmail.com>
2017-09-02 15:51:25 -0700

    updating format and group values

--- src/js/jet-composites/my-chart/viewModel.js
+++ src/js/jet-composites/my-chart/viewModel.js
@@ -29,9 +29,9 @@ define(
                          {name: "Series 2", items: [55, 30]},
                          {name: "Series 3", items: [36, 50]},
                          {name: "Series 4", items: [22, 46]},
-                         {name: "Series 5", items: [22, 46]}];
+                         {name: "Series 5", items: [15, 33]}];
     
-        var barGroups = ["Group A", "Group B"];
+        var barGroups = ["2015 Results", "2017 Results"];
    
         self.barSeriesValue = ko.observableArray(barSeries);
         self.barGroupsValue = ko.observableArray(barGroups);
