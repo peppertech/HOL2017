cd2b88e69133672a075c952deb84280925021fec
peppertech <peppertech@gmail.com>
2018-01-16 14:43:34 -0800

    switching requirejs config to use CDN instead of local files

--- adv-finished/src/js/main.js
+++ adv-finished/src/js/main.js
@@ -5,51 +5,6 @@
 'use strict';
 
 /**
- * Example of Require.js boostrap javascript
- */
-
-requirejs.config(
-{
-  baseUrl: 'js',
-
-  // Path mappings for the logical module names
-  // Update the main-release-paths.json for release mode when updating the mappings
-  paths:
-  //injector:mainReleasePaths
-  {
-    'knockout': 'libs/knockout/knockout-3.4.0.debug',
-    'jquery': 'libs/jquery/jquery-3.1.1',
-    'jqueryui-amd': 'libs/jquery/jqueryui-amd-1.12.0',
-    'promise': 'libs/es6-promise/es6-promise',
-    'hammerjs': 'libs/hammer/hammer-2.0.8',
-    'ojdnd': 'libs/dnd-polyfill/dnd-polyfill-1.0.0',
-    'ojs': 'libs/oj/v4.0.0/debug',
-    'ojL10n': 'libs/oj/v4.0.0/ojL10n',
-    'ojtranslations': 'libs/oj/v4.0.0/resources',
-    'text': 'libs/require/text',
-    'signals': 'libs/js-signals/signals',
-    'customElements': 'libs/webcomponents/custom-elements.min',
-    'proj4': 'libs/proj4js/dist/proj4-src',
-    'css': 'libs/require-css/css',
-		'socketio': 'libs/socketio-client/socket.io'
-  }
-  //endinjector
-  ,
-  // Shim configurations for modules that do not expose AMD
-  shim:
-  {
-    'jquery':
-    {
-      exports: ['jQuery', '$']
-    },
-		'socketio': {
-      exports: 'io'
-    }
-  }
-}
-);
-
-/**
  * A top-level require call executed by the Application.
  * Although 'ojcore' and 'knockout' would be loaded in any case (they are specified as dependencies
  * by the modules themselves), we are listing them explicitly to get the references to the 'oj' and 'ko'
@@ -59,7 +14,7 @@ require(['ojs/ojcore', 'knockout', 'appController', 'ojs/ojknockout',
   'ojs/ojmodule', 'ojs/ojrouter', 'ojs/ojnavigationlist', 'ojs/ojbutton', 'ojs/ojtoolbar'],
   function (oj, ko, app) { // this callback gets executed when all required modules are loaded
 
-    $(function() {
+    $(function () {
 
       function init() {
         oj.Router.sync().then(
@@ -83,5 +38,74 @@ require(['ojs/ojcore', 'knockout', 'appController', 'ojs/ojknockout',
 
     });
 
+  }
+);
+
+/**
+ * Example of Require.js boostrap javascript using JET CDN
+ */
+
+
+function _getCDNPath(paths) {
+  var cdnPath = "https://static.oracle.com/cdn/jet/";
+  var ojPath = "v4.1.0/default/js/";
+  var thirdpartyPath = "v4.1.0/3rdparty/";
+  var keys = Object.keys(paths);
+  var newPaths = {};
+  function _isoj(key) {
+    return (key.indexOf('oj') === 0 && key !== 'ojdnd');
+  }
+  keys.forEach(function (key) {
+    newPaths[key] = cdnPath + (_isoj(key) ? ojPath : thirdpartyPath) + paths[key];
+  });
+  return newPaths;
+}
+
+requirejs.config({
+  paths: _getCDNPath({
+    'knockout': 'knockout/knockout-3.4.0.debug',
+    'jquery': 'jquery/jquery-3.1.1',
+    'jqueryui-amd': 'jquery/jqueryui-amd-1.12.0.min',
+    'promise': 'es6-promise/es6-promise.min',
+    'ojs': 'debug',
+    'ojL10n': 'ojL10n',
+    'ojtranslations': 'resources',
+    'signals': 'js-signals/signals.min',
+    'text': 'require/text',
+    'hammerjs': 'hammer/hammer-2.0.8.min',
+    'ojdnd': 'dnd-polyfill/dnd-polyfill-1.0.0.min',
+    'css': 'require-css/css',
+    'customElements': 'webcomponents/custom-elements.min'
+  }),
+  // Shim configurations for modules that do not expose AMD
+  shim: {
+    'jquery': {
+      exports: ['jQuery', '$']
+    }
+  }
+});
+
+
+/* 
+Adding local libraries that are not available on the CDN
+*/
+
+requirejs.config(
+  {
+    baseUrl: 'js',
+
+    // Path mappings for the logical module names
+    paths:
+      {
+        'socketio': 'libs/socketio-client/socket.io'
+      }
+    ,
+    // Shim configurations for modules that do not expose AMD
+    shim:
+      {
+        'socketio': {
+          exports: 'io'
+        }
+      }
   }
 );
